8e860da9dc8cd02f03b9a78940ae966be1da4e64
kimomom1547 <kimomom1547@gmail.com>
2026-09-17 12:22:29 +0900

    supabase connect done

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -76,12 +79,31 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+
+        "ENGINE":
+            "django.db.backends.postgresql",
+
+        "NAME":
+            os.getenv("DB_NAME"),
+
+        "USER":
+            os.getenv("DB_USER"),
+
+        "PASSWORD":
+            os.getenv("DB_PASSWORD"),
+
+        "HOST":
+            os.getenv("DB_HOST"),
+
+        "PORT":
+            os.getenv("DB_PORT"),
+
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
